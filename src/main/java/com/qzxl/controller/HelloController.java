db993d58f45ecb80d4b8d89212bdfdc564bd4b63
qccp <wqz**10086>
2020-04-06 21:02:37 +0800

    添加年龄

--- src/main/java/com/qzxl/controller/HelloController.java
+++ src/main/java/com/qzxl/controller/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
     @RequestMapping("/")
     public String sayHello(){
         String name="wqz:today,";
-        int age=19;
+        int age=18;
         return name+"jenkins,hello!,今年是"+age+"岁";
     }
 }
