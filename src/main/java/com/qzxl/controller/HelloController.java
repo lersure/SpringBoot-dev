f4822da4469d994d28333514730d767849a60235
qccp <wqz**10086>
2020-04-06 19:36:25 +0800

    添加姓名

--- src/main/java/com/qzxl/controller/HelloController.java
+++ src/main/java/com/qzxl/controller/HelloController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/")
     public String sayHello(){
-        String name="wqz:";
+        String name="wqz:today,";
         return name+"jenkins,hello!";
     }
 }
